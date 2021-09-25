61bcb49f01b64c0c27aad8674df60defcfe93a8a
Joseph Baldera <josephbalderaa@gmail.com>
2021-09-25 01:46:39 -0500

    Nuevos cambios

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,23 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection', (client) => {
     console.log('Cliente conectado');
 
+    const [valido, uid] = comprobarJWT( client.handshake.headers['x-token'] );
+
+    if( !valido ) { return client.disconnect(); }
+
+    //Cliente autenticado
+    usuarioConectado( uid );
+
+    console.log('cliente autenticado');
+
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        usuarioDesconectado( uid );
     });
 
     // client.on('mensaje', ( payload ) => {
