c787f21be034486951c750ce673e7fd8e684abd6
Joseph Baldera <josephbalderaa@gmail.com>
2021-09-25 16:45:07 -0500

    Fin Backend

---
+++ controllers/mensajes.js
@@ -0,0 +1,25 @@
+
+const mensaje = require('../models/mensaje');
+const Mensaje = require('../models/mensaje');
+
+const obtenerChat = async(req, res) => {
+
+    const miId = req.uid;
+    const mensajesDe = req.params.de;
+
+    const last30 = await Mensaje.find({
+        $or: [{ de: miId, para: mensaje }, { de: mensajesDe, para: miId }]
+    })
+    .sort({ createdAt: 'desc' })
+    .limit(30);
+
+    res.json({
+        ok: true,
+        mensajes: last30
+    })
+
+}
+
+module.exports = {
+    obtenerChat
+}
